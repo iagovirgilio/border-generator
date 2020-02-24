461208dc21e28e4e2154a7f2eb432623c2c99c38
Iago Virgílio <iagovirgilio@gmail.com>
2020-02-24 17:51:02 -0300

    Versao 1.0

--- assets/indes.js
+++ assets/indes.js
@@ -1,4 +1,4 @@
-const generateButton = document.querySelector('button');
+const generateButton = document.getElementById('generate');
 
 generateButton.addEventListener('click', () => {
     const borderElement = document.getElementById('border');
@@ -8,10 +8,25 @@ generateButton.addEventListener('click', () => {
     let borderBottomRightRadius = document.getElementById('border-botton-right').value;
 
     let paramsBorder = document.getElementById('params-border');
-    paramsBorder.innerHTML = 'border-radius: ' + borderBottomLeftRadius + 'px;';
-
+    if (borderBottomLeftRadius == borderBottomRightRadius && borderBottomLeftRadius == borderTopRightRadius && borderBottomLeftRadius == borderTopLeftRadius) {
+        paramsBorder.innerHTML = 'border-radius: ' + borderBottomLeftRadius + 'px;';
+    }
+    else {
+        paramsBorder.innerHTML = 'border-top-left-radius: ' + borderTopLeftRadius + 'px;\n' +
+            'border-top-right-radius: ' + borderTopRightRadius + 'px;\n' +
+            'border-bottom-left-radius: ' + borderBottomLeftRadius + 'px;\n' +
+            'border-bottom-rigth-radius: ' + borderBottomRightRadius + 'px;';
+    }
     borderElement.style.borderBottomLeftRadius = borderBottomLeftRadius + 'px';
     borderElement.style.borderBottomRightRadius = borderBottomRightRadius + 'px';
     borderElement.style.borderTopLeftRadius = borderTopLeftRadius + 'px';
     borderElement.style.borderTopRightRadius = borderTopRightRadius + 'px';
+});
+
+const copyButton = document.getElementById('copy');
+
+copyButton.addEventListener('click', () => {
+    let textarea = document.getElementById('params-border');
+    textarea.select();
+    document.execCommand("copy");
 });
